ee3a9de79a1ab67fe0f99132a0b9ff368dd184fd
Andrew_Levitsky <andreilevitsky23@gmail.com>
2022-01-25 01:01:25 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//  const category = 'toys';
+
+//  console.log('http://someurl.com/'+ category);
+
+// const user = "Andrew";
+
+// alert(`Hi, my name, ${user}`);
+
+// let incr = 10,
+//   decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(2+2*2 == "6");
+
