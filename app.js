46be5cbd2bc868c54076817c0bce0851d00c1b97
Andrew_Levitsky <andreilevitsky23@gmail.com>
2022-05-18 11:47:52 +0300

    18.05.2022

--- app.js
+++ app.js
@@ -3,10 +3,18 @@
 let number1 = 5; //declaration of a variable and its value (changeable)
 const leftBorderWidth = 1; //declaration of a variable and its value (not changeable)
 console.log(number1); //output the result of the command to the terminal console
+/*
+ autoSizeWidth - 'CamelCase'
+ auto_size_width - 'Snake_Case'
+ AUTO_SIZE_WIDTH - 'APR_Case', used for constants!
+ _autoSizeWidth - '__CamelCase' used for constants!
+ auto-size-width - 'Kebab-Case'
+ AutoSizeWidth - 'PascalCase', to designate classes
+*/
 //_____________________________________________________________________________data type___________________________
-let number = 4.6; //numeric data type
-console.log(4 / 0); //'Infinity'
-console.log("string" * 9); //'NaN' not a namber
+let number = 4.6; // numeric data type
+console.log(4 / 0); // 'Infinity'
+console.log("string" * 9); //'NaN' - not a namber
 
 let persone = "Andrew"; //string data type (written only in quotes!)
 persone = "777"; //also the string
@@ -14,6 +22,7 @@ persone = "777"; //also the string
 let bool = false; //boolean data type ('yes' or 'no')
 bool = true;
 //console.log(something); //'NaN' reference to a non-existent object
+
 let und;
 console.log(und); //'undefined' empty object
 
@@ -28,6 +37,12 @@ const obj = {
 let arr = ["plum.png", "orange.jpg", 6, "apple.bmb"]; //an array is a special case of an object
 //array, all elements have a serial number and are separated by commas, puts any data types
 console.log(arr[1]); //accessing element number 1 'oranhe.jpg'
+
+const ARR = [1, 2, 3]; //array, list of information in order
+console.log(ARR[1]); //get "2"
+
+const OBJ = { a: 1, b: 2 }; //object, data storage structure in pair format, key-value
+OBJ.C = "123445"; //added a new property to the object
 
 //______________________________________________________________communication with the user____________________________
 
@@ -49,7 +64,7 @@ console.log(typeof answers); //output to console
 //______________________________________________________________________interpolation______________________________
 
 const category = "toys"; //Dynamic interpolation of variable value in unlimited number
-console.log(`https://someurl.com/${category}`);
+console.log(`https://someurl.com/${category}`); //bebtics are used ``
 
 const user = "Andrewlev";
 alert(`Привет, ${user}`);
@@ -210,7 +225,7 @@ console.log(str1[3]); //will output the letter 't' from 'test'
 const logg = "Hello Petro";
 console.log(logg.slice(6, 11)); // cut out the word 'Petro'
 
-console.log(logg.substring(6, 11)); //будет тоже самое
+console.log(logg.substring(6, 11)); //will be the same
 
 const num4 = 12.2;
 console.log(Math.round(num4)); //rounding to the nearest whole number
@@ -283,7 +298,7 @@ for (let key in options) {
   }
 }
 
-//______________________________________________________________arrays and imaginary arrays____________________________
+//______________________________________________________________arrays and false-arrays  ____________________________
 
 const arr2 = [2, 3, 6, 8, 10];
 //arr2.pop(); //removes the last element from the array
@@ -291,18 +306,18 @@ const arr2 = [2, 3, 6, 8, 10];
 console.log(arr2);
 
 for (let i = 0; i < arr2.length; i++) {
-  //the loop will work until the objects inside the array run out
+  //the cycle will work until the objects inside the array run out
   console.log(arr2[i]); //to access an element of an array, you need to put it in '[]'
 }
 
 for (let value of arr2) {
-  //enumeration design 'for...of' ('value' display of each individual element) can be stopped with the 'break' command
+  //search method 'for...of' ('value' display of each individual element) can be stopped with the 'break' command
   console.log(value); //it turns out the same result as in the above array, it works only with arrays!
 }
 let arr3 = [2, 3, 6, 8, 9];
 arr3.forEach(function (item, i, arr3) {
   console.log(`${i}: ${item} inside an array ${arr3}`);
-}); //method, meeting to iterate over all elements inside an array
+}); //method, allowing iteration of all elements inside the array
 
 const str = prompt("", "");
 const products = str.split(", "); //created an array of strings
@@ -313,22 +328,324 @@ function compareNum(a, b) {
   return a - b;
 }
 
-//______________________________________________________________Prototyping Legacy__________________________________
-let str3 = "some";
-//let str3Obj = new String(str3); //created an object which turned back into a string
+//______________________________________________________________pass by reference, "spread" operator__________________
 
-console.log(typeof str3);
-console.log(typeof str3Obj);
+function copy(mainObj) {
+  //this function with a cycle, will create a new object with the old properties
+  let objCopy = {};
+  let key;
+  for (key in mainObj) {
+    objCopy[key] = mainObj[key];
+  }
+  return objCopy;
+}
 
-console.dir([1, 2, 3]);
+const numbers = {
+  a: 2,
+  b: 5,
+  c: {
+    x: 7,
+    y: 4,
+  },
+};
+
+const nweNumbers = copy(numbers); //copied array "numbers" with all properties
+
+nweNumbers.a = 10; //changed the property in the copy of the array
+nweNumbers.c.x = 100; //does not change! nested arrays do not change!
+//______second way
+const add = {
+  d: 17,
+  e: 20,
+};
+const clone = Object.assign({}, add); //I put the numbers object in a new empty object
+clone.d = 20;
+
+console.log(add);
+console.log(clone);
+//______third way
+const oldArray = ["a", "b", "c"];
+const newArray = oldArray.slice();
+
+newArray[1] = "h";
+console.log(newArray);
+console.log(oldArray);
+//_____fourth method ('spread' operator)
+const video = ["youtube", "vimeo", "rurube"],
+  blogs = ["wordpress", "livejournal", "blogger"],
+  internet = [...video, ...blogs, "vk", "facebook"]; //operator '...' inserts other loops
+
+console.log(internet); //this operator also works for objects
+
+const arrayyy = ["a", "b"];
+const newArrayyy = [...arrayyy]; //copied the properties of the previous array
+
+//_______________________________________________________________________________basics of "OOP"____________________
 
 const soldier = {
   health: 400,
   armor: 100,
-  sayHello: function () {
-    console.log("Hello");
-  },
 };
 
-const jonh = Object.create(soldier); //allows you to take properties from the object's prototype
-jonh.sayHello();
+const jonh = {
+  health: 100,
+};
+
+Object.setPrototypeOf(jonh, soldier); //assign properties to john from soldier prototype
+console.log(jonh.armor); //'100', took over the property from the prototype
+
+const jonh2 = Object.create(soldier); //second improved way to adopt properties from a prototype
+console.log(jonh2.armor); // '100'
+//____________________________________________________________________deleting errors in the console. Breakpoints_____
+
+/*In the console go to the folder "Sources". In this tab, you can set
+ breakpoints in any of the lines to check a specific section of the code
+
+ function Hello() {
+  console.log("Hello World");
+  debugger; //stop code debugging in console!
+} */
+//____________________________________________________________________dynamic typing in JS_____________________________
+
+//To String
+console.log(typeof String(null)); //turned "null" into a string
+console.log(typeof String(44)); //...the same
+
+console.log(typeof (6 + "")); //...the same
+
+const num5 = 5;
+console.log("https://vk.com/catalog/" + num); //added a number to the string
+
+const fontSize = 26 + "px"; //...the same
+
+//To Number
+console.log(typeof Number("7")); //turned "string" into a number
+console.log(typeof +"7"); //...the same
+let answ = +prompt("Hello", ""); //...the same
+
+//To boolean
+// 1)
+//0, '', undefined, NaN - false!
+let swicher = null; // 'false'. Condition not will work!
+if (swicher) {
+  console.log("Working...");
+}
+
+swicher = 1; //turned 'false' in the 'true'. Condition will work!
+if (swicher) {
+  console.log("Working...");
+}
+// 2)
+console.log(typeof Boolean("9")); //turned in console - 'boolean'
+// 3)
+console.log(typeof !!"9"); //...the same. Operator '!!'
+
+//____________________________________________________________________tasks from interviews_____________________________
+//1
+let x = 5;
+alert(x++); //5, postfix
+let xx = 5;
+alert(++xx); //6, prefix
+//2
+// [] + false + null + true;  = NaN
+console.log([] + false); //'false'
+console.log("false" + null); //NaN
+console.log(NaN + true); //NaN
+//3
+/* let p = 1;
+   let x1 = p = 2;
+   alert(x1);  = 2   */
+//4
+console.log([] + 1 + 2); // =12 ...  [ ] == " "
+//5
+alert("1"[0]); // =1
+//6
+console.log(2 && 1 && null && 0 && undefined); // null (stops at lies)
+//7
+// console.log(!!(k && g) == (k && g)); // first boolean, second string "!="
+//8
+alert(null || (2 && 3) || 4); // = 3 ,('||' stops at true)
+//9
+const g = [1, 2, 3];
+const l = [1, 2, 3];
+//g !== l; since these are two separate cycles!
+//10
+alert(+"Infinity");
+// Infinity (number data type)
+//11
+console.log("Ежик" > "Яблоко"); //Folse!
+//12
+console.log(0 || "" || 2 || undefined || true || false);
+//  = 2 (stops at true)
+//____________________________________________________________________getting elements from the page____________________
+
+const Box = document.getElementById("Box");
+console.log(Box); //pulled an ELEMENT from the page by ID
+
+const Btns = document.getElementsByTagName("Button")[1];
+console.log(Btns[1]); //pulled the TAG from the page by ID (pseudo array)
+
+const Circles = document.getElementsByClassName("Circles");
+console.log(Circles); //pulled the CLASS from the page by ID
+
+const Hearts = document.querySelectorAll(".Heart");
+console.log(Hearts); //extract CSS selector from document
+Hearts.forEach((item) => {
+  console.log(item);
+}); //output one by one all the elements in the 'hearts' array
+
+const OneHeart = document.querySelector(".Heart");
+console.log(OneHeart); //get only one element from the page
+//____________________________________________________________________action on elements on the page____________________
+
+const box = document.getElementById("box"),
+  btns = document.getElementsByTagName("button")[1],
+  circles = document.getElementsByClassName("circle"),
+  hearts = document.querySelectorAll(".heart"),
+  oneHeart = document.querySelector(".heart"),
+  wrapper = document.querySelector(".wrapper");
+
+console.dir(box); //checking object properties
+
+box.style.backgroundColor = "blue"; //changing color properties
+box.style.width = "500px"; //changing width properties
+
+btns[1].style.borderRadius = "100%"; //change properties of roundings
+circles.style.backgroundColor = "red";
+
+box.style.cssText = `background-color: blue; width: 500px`; //changing text properties
+
+hearts.forEach((item) => {
+  item.style.backgroundColor = "blue";
+}); //changing the properties of multiple elements
+
+const div = document.createElement("div"); //created element inside JS
+const text = document.createTextNode("Тут был я"); //outputting text to a document
+
+wrapper.prepend(div); //moves the block up
+
+hearts[0].before(div); //put a block before the specified element
+hearts[0].after(div); //put a block after the specified element
+
+circles[0].remove(); //removing an element from a page
+
+hearts[0].replaceWith(circles[0]); //replacing an element with another
+
+div.innerHTML = "Hello Piska"; //insert text into element
+div.innerHTML = "<h1>HELLO PISKA</h1>"; //you can also insert HTML structure
+
+div.insertAdjacentHTML("afterbegin", "<h2>Who are you bitch</h2>"); //put text at the beginning of the element
+div.insertAdjacentHTML("afterand", "<h2>Who are you bitch</h2>"); //put text after element
+
+//_________________________________________________________________________events and handlers_________________________
+
+const btn = document.querySelector("button"),
+  overlay = document.querySelector(".overlay"); //nested event
+
+btn.addEventListener("click", () => {
+  alert("Click"); //perform action on click
+});
+
+btn.addEventListener("mouseenter", () => {
+  alert("Hover"); //perform mouse hover action
+});
+
+btn.addEventListener("mouseenter", (e) => {
+  console.log(e); //getting the properties and contents of an object
+  console.log(e.target); //triggering an event on a specific element
+  e.target.remove(); //removed a specific property from an object
+});
+
+let i = 0;
+const deleteElement = (e) => {
+  console.log(e.target);
+  i++;
+  if (i == 1) {
+    //condition ends
+    btn.removeEventListener("Click", deleteElement); //remove event handler
+  }
+};
+
+const deleteElement1 = (e) => {
+  console.log(e.target);
+  console.log(e.type);
+};
+overlay.addEventListener("Click", deleteElement);
+/* fires first because it is nested more than the parent this is called "popup event", 
+ when this action fires first on a nested element and then rises to the top of the hierarchy of our DOM tree */
+
+const link = document.querySelector("a");
+
+link.addEventListener("click", (event) => {
+  event.preventDefault(); //canceled the default action!
+  console.log(event.target);
+}); //callback function to override default browser behavior
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", deleteElement);
+}); //assigned the same event handler to multiple elements at once!
+
+//_________________________________________________________________________DOM elements navigation____________________
+
+console.log(document.head); //let's jump to the file header
+console.log(document.documentElement); //let's go to the whole file
+console.log(document.body.childNodes); //go to the child from the "body"
+
+console.log(document.body.firstChild); //having a first child 'body'
+console.log(document.body.lastChild); //getting the last child 'body'
+
+console.log(document.querySelector("#currenr").parentNode); //get parent '#current'
+
+console.log(document.querySelector("[data-current='3']").nextSibling); //line break
+
+console.log(document.querySelector("[data-current='3']").previousSibling); //get previous neighbor
+console.log(document.querySelector("[data-current='3']").nextElementSibling); //get next neighbor
+
+for (let node of document.body.childNodes) {
+  if (node.nodeName == "#text") {
+    continue; //stop cicle iteration
+  }
+  console.log(node);
+}
+
+//_________________________________________________________________________events on mobile devices____________________
+
+// touchstart - возникновение касания к елементу
+// touchmove - если палец при касании смещается с элемента
+// touchend - палец оторвался от элемента
+// touchenter - ведем палец по экрану и попадаем на элемент
+// touchleave - палец вышел за пределы элемента
+// touchcancel - палец вышел за пределы браузера
+
+window.addEventListener("DOMContentLoaded", () => {
+  //большой обработчик события, прогружает все 'DOM' узлы
+  const box = document.querySelector(".box"); //вытянул с верстки элемент 'box'
+
+  box.addEventListener("touchstart", (e) => {
+    //оброботчик событий связанный с мобильными устройствами
+    e.preventDefault(); //отмена стандартного поведения браузера
+    console.log("Start");
+    console.log("e.touches");
+  });
+});
+
+//toches - выдает список всех пальцев, которые взаимодействуют с экраном
+//targetTouches - выдает количество пальцев, которые взаимодействуют с элементом
+//changedTouches - список пальцев, которые учавствуют в текущем событии
+
+//_____________________________________________________________________Async, defer, dynamic scripts________________
+
+//Always place scripts at the end of the document!
+
+//<script defer src='js/script.js'></script>;  - waits for the layout to be ready, after that it launches JS
+const p = document.querySelectorAll("p");
+console.log(p);
+
+//<script async src='js/script.js'></script>;  - does not depend on the DOM structure, works immediately after loading
+const t = document.querySelectorAll("t");
+console.log(t);
+
+const script = document.createElement("script"); // dynamic TAG-script
+script.src = "js/test.js"; //link to 'JS' file
+script.async = false; //reversed the async behavior
+document.body.append(script); //placing an element on the page
